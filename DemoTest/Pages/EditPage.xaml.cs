b5e1c998c2e940da5ee89faa3f66e63065654a7f
vinczforte <vinczforte>
2021-06-22 00:45:45 +0300

    staaas

---
+++ DemoTest/Pages/EditPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoTest.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для EditPage.xaml
+    /// </summary>
+    public partial class EditPage : Page
+    {
+        public Client ContClient;
+        public EditPage(Client selClient)
+        {
+            InitializeComponent();
+            ContClient = selClient;
+            this.DataContext = ContClient;
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ContClient.ID == 0) 
+            {
+                MainWindow.db.Client.Add(ContClient);
+            }
+            MainWindow.db.SaveChanges();
+            MessageBox.Show("Клиент добавлен");
+            NavigationService.Navigate(new TablePage());
+        }
+    }
+}
---
+++ DemoTest/Pages/TablePage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DemoTest.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для TablePage.xaml
+    /// </summary>
+    public partial class TablePage : Page
+    {
+        public TablePage()
+        {
+            InitializeComponent();
+        }
+
+        private void DgridView_Loaded(object sender, RoutedEventArgs e)
+        {
+            DgridView.ItemsSource = MainWindow.db.Client.ToList();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new EditPage(new Client()));
+        }
+
+        private void BtnDel_Click(object sender, RoutedEventArgs e)
+        {
+            var selClient = DgridView.SelectedItem as Client;
+
+            if (MessageBox.Show("Вы уверены что хотите безвозратно удалить запись?",
+                "Предупреждение об удалении", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                MainWindow.db.Client.Remove(selClient as Client);
+                MainWindow.db.SaveChanges();
+                UpdateList();
+                MessageBox.Show("Удалено успешно!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            var selClient = DgridView.SelectedItem as Client;
+            if (selClient == null)
+            {
+                MessageBox.Show("Ничего не выбрано");
+                return;
+            }
+            else
+                NavigationService.Navigate(new EditPage(selClient));
+        }
+        public void UpdateList()
+        {
+            DgridView.ItemsSource = null;
+            DgridView.ItemsSource = MainWindow.db.Client.ToList();
+        }
+    }
+}
